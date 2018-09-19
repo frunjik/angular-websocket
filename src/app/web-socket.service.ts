f5f9cb245d22431ca9efcc2fd8266b3277126eae
frunjik <frunjik@gmail.com>
2018-09-19 23:09:15 +0200

    send string and reuse behaviour

--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -1,4 +1,3 @@
-
 // https://vladimir-ivanov.github.io/testing/websocket/with/rxjs/and/angular.io/2017/09/13/websocket-with-angular.io-and-rxjs.html
 
 import {Inject, Injectable} from '@angular/core';
@@ -8,21 +7,11 @@ import {Observer} from "rxjs/Observer";
 
 @Injectable()
 export class WebSocketService {
-  private subject: Subject<MessageEvent>;
 
   constructor(@Inject(Window) private window) {
   }
 
   public connect(url: string): Subject<MessageEvent> {
-    /* JvH: There is a problem when opening a new connection to different url ??? */
-    if (!this.subject) {
-      this.subject = this.create(url);
-    }
-
-    return this.subject;
-  }
-
-  private create(url: string): Subject<MessageEvent> {
     const ws = new this.window.WebSocket(url);
 
     const observable = Observable.create((obs: Observer<MessageEvent>) => {
@@ -34,9 +23,9 @@ export class WebSocketService {
     });
 
     const observer = {
-      next: (data: Object) => {
+      next: (data: String) => {
         if (ws.readyState === this.window.WebSocket.OPEN) {
-          ws.send(JSON.stringify(data));
+          ws.send(data);
         }
       }
     };
